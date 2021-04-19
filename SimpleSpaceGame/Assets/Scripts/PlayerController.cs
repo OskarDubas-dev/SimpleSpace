65670cc9435d12fab7927f6504a5f74693134207
OskarDubas-dev <oohdee.dev@gmail.com>
2021-04-19 11:27:58 +0100

    Starting on player controller plus small bits
    + background
    + player controller
    + input controller

---
+++ SimpleSpaceGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+   
+
+   void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+}
