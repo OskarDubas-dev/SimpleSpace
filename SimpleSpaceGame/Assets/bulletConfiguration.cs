a1452a91a886534581cb4dcceb1aa08e4e093e2e
OskarDubas-dev <oohdee.dev@gmail.com>
2021-05-06 09:47:18 +0100

    Bullets destroy function
    bullets are destroyed after certain time

--- SimpleSpaceGame/Assets/bulletConfiguration.cs
+++ SimpleSpaceGame/Assets/bulletConfiguration.cs
@@ -7,11 +7,19 @@ public class bulletConfiguration : MonoBehaviour
 
     [SerializeField] float projectileSpeed = 9f;
     float rotationSpeed = 25f;
+    float timeBeforeDestroyed = 2.0f;
 
+    GameObject bullet;
+
+    private void Awake()
+    {
+        bullet = this.gameObject;
+    }
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        StartCoroutine(deleteItself());
         
     }
 
@@ -20,5 +28,11 @@ public class bulletConfiguration : MonoBehaviour
     {
 
         transform.Rotate(0, 0, rotationSpeed);
+    }
+
+    IEnumerator deleteItself()
+    {
+        yield return new WaitForSeconds(timeBeforeDestroyed);
+        Destroy(bullet);
     }
 }
