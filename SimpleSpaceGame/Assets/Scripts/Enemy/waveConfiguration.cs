4dd1f1ed652dfc4b792f3fd29b038d6c73ee7ffd
OskarDubas-dev <oohdee.dev@gmail.com>
2021-05-13 14:30:35 +0100

    wave Scriptable Object

---
+++ SimpleSpaceGame/Assets/Scripts/Enemy/waveConfiguration.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class waveConfiguration : ScriptableObject
+{
+    [field: SerializeField] public GameObject enemyPrefab { get; private set; }
+    [field: SerializeField] public GameObject pathPrefab { get; private set; }
+    [field: SerializeField] public float timeBetweenSpawns { get; private set; } = 0.5f;
+    [field: SerializeField] public int numberOfEnemies { get; private set; } = 5;
+    [field: SerializeField] public float moveSpeed { get; private set; } = 2f;
+
+
+}
